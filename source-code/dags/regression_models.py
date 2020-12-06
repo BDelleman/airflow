13df842309739b7f04118cca067d92cb700dbd28
Bart Delleman <bart.delleman@virtualsciences.nl>
2020-12-06 23:24:56 +0100

    Build for other notebooks

--- source-code/dags/regression_models.py
+++ source-code/dags/regression_models.py
@@ -30,14 +30,14 @@ with models.DAG(
     )
     data_preprocessing = PapermillOperator(
         task_id='data_preprocessing',
-        input_nb=input_bucket+'/data_preprocessing.ipynb',
+        input_nb=input_bucket+'/notebooks/data_preprocessing.ipynb',
         output_nb='/home/airflow/gcs/data/data_preprocessing_out.ipynb',
         parameters={},
     )
 
     multi_linear_regression = PapermillOperator(
         task_id='multi_linear_regression',
-        input_nb=input_bucket+'/multi_linear_regression.ipynb',
+        input_nb=input_bucket+'/notebooks/multi_linear_regression.ipynb',
         output_nb='/home/airflow/gcs/data/multi_linear_regression_out.ipynb',
         parameters={},
         dag=dag
@@ -45,7 +45,7 @@ with models.DAG(
 
     random_forest_regression = PapermillOperator(
         task_id='random_forest_regression',
-        input_nb=input_bucket+'/random_forest_regression.ipynb',
+        input_nb=input_bucket+'/notebooks/random_forest_regression.ipynb',
         output_nb='/home/airflow/gcs/data/random_forest_regression_out.ipynb',
         parameters={},
     )
