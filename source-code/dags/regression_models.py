db8e66b24934361752251b17b5cdfc3bfefc460e
Bart Delleman <bart.delleman@virtualsciences.nl>
2020-12-08 11:18:05 +0100

    finished dag

--- source-code/dags/regression_models.py
+++ source-code/dags/regression_models.py
@@ -10,8 +10,6 @@ region = models.Variable.get('gcp_region')
 zone = models.Variable.get('gcp_zone')
 input_bucket = 'gs://' + models.Variable.get('gcs_input_bucket_test')
 output_bucket_name = models.Variable.get('gcs_output_bucket_test')
-output_bucket = 'gs://' + output_bucket_name
-output_prefix = 'output'
 
 default_args = {
     'start_date': airflow.utils.dates.days_ago(0),
@@ -24,14 +22,16 @@ default_args = {
 with models.DAG(
     'regression_models',
     default_args=default_args) as dag:
-    check_buckets = BashOperator(
-        task_id='check_buckets',
-        bash_command='echo $input_bucket',
+    data_preprocessing = PapermillOperator(
+        task_id='data_preprocessing',
+        input_nb=input_bucket+'notebooks/data_preprocessing.ipynb',
+        output_nb='/home/airflow/gcs/data/data_preprocessing_out.ipynb',
+        parameters={},
     )
 
     multi_linear_regression = PapermillOperator(
         task_id='multi_linear_regression',
-        input_nb='/home/airflow/gcs/data/notebooks/multi_linear_regression.ipynb',
+        input_nb=input_bucket'notebooks/multi_linear_regression.ipynb',
         output_nb='/home/airflow/gcs/data/multi_linear_regression_out.ipynb',
         parameters={},
         dag=dag
@@ -39,7 +39,7 @@ with models.DAG(
 
     random_forest_regression = PapermillOperator(
         task_id='random_forest_regression',
-        input_nb='/home/airflow/gcs/data/notebooks/random_forest_regression.ipynb',
+        input_nb=input_bucket'/notebooks/random_forest_regression.ipynb',
         output_nb='/home/airflow/gcs/data/random_forest_regression_out.ipynb',
         parameters={},
     )
