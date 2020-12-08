8d7a6e174e028aa3a8a0d5d87e9bdbabcd07956e
Bart Delleman <bart.delleman@virtualsciences.nl>
2020-12-08 10:14:05 +0100

    notebooks from local airflow dir

--- source-code/dags/regression_models.py
+++ source-code/dags/regression_models.py
@@ -30,14 +30,14 @@ with models.DAG(
     )
     data_preprocessing = PapermillOperator(
         task_id='data_preprocessing',
-        input_nb=input_bucket+'/notebooks/data_preprocessing.ipynb',
+        input_nb='/home/airflow/gcs/data/notebooks/data_preprocessing.ipynb',
         output_nb='/home/airflow/gcs/data/data_preprocessing_out.ipynb',
         parameters={},
     )
 
     multi_linear_regression = PapermillOperator(
         task_id='multi_linear_regression',
-        input_nb=input_bucket+'/notebooks/multi_linear_regression.ipynb',
+        input_nb='/home/airflow/gcs/data/notebooks/multi_linear_regression.ipynb',
         output_nb='/home/airflow/gcs/data/multi_linear_regression_out.ipynb',
         parameters={},
         dag=dag
@@ -45,27 +45,9 @@ with models.DAG(
 
     random_forest_regression = PapermillOperator(
         task_id='random_forest_regression',
-        input_nb=input_bucket+'/notebooks/random_forest_regression.ipynb',
+        input_nb='/home/airflow/gcs/data/notebooks/random_forest_regression.ipynb',
         output_nb='/home/airflow/gcs/data/random_forest_regression_out.ipynb',
         parameters={},
     )
 
-"""
-test
-evaluate_models = PapermillOperator(
-    task_id='data_processing',
-    input_nb='/home/airflow/gcs/data/evaluate_models.ipynb',
-    output_nb='/home/airflow/gcs/data/evaluate_models_out.ipynb',
-    parameters={},
-    dag=dag
-)
-
-cleanup = PapermillOperator(
-    task_id='data_processing',
-    input_nb='/home/airflow/gcs/data/cleanup.ipynb',
-    output_nb='/home/airflow/gcs/data/cleanup_out.ipynb',
-    parameters={},
-    dag=dag
-)
-"""
 check_buckets >> data_preprocessing >> multi_linear_regression >> random_forest_regression
